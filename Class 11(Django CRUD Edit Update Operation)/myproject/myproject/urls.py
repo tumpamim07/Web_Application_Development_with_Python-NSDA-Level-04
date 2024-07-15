22ff841d3a716793be4c8fd0d5be53eb58e55369
Tumpa Moni Mim <tumpamoni455@gmail.com>
2024-07-15 23:06:15 +0600

    Django CRUD Edit Update Operation
    -Edit Operation
    -Update Operation

---
+++ Class 11(Django CRUD Edit Update Operation)/myproject/myproject/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from myproject.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', home,name='home'),
+    path('studentpage/', studentpage,name='studentpage'),
+    path('addstudentpage/', addstudentpage,name='addstudentpage'),
+    path('deletestudentpage/<str:myid>', deletestudentpage,name='deletestudentpage'),
+    path('editstudentpage/<str:myid>', editstudentpage,name='editstudentpage'),
+    path('updatestudentpage/', updatestudentpage,name='updatestudentpage'),
+    
+]
---
+++ Class 11(Django CRUD Edit Update Operation)/myproject/myproject/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render,redirect,HttpResponse
+from myapp.models import *
+
+def home(request):
+    return render(request,'home.html')
+
+def studentpage(request):
+    student=StudentModel.objects.all()
+    mydic={
+        'std':student
+    }
+    return render(request,'studentpage.html',mydic)
+
+def addstudentpage(request):
+
+    if request.method=='POST':
+        fname=request.POST.get('firstname')
+        lname=request.POST.get('lastname')
+        dep=request.POST.get('department')
+        cou=request.POST.get('country')
+
+        stdn=StudentModel(
+            FirstName=fname,
+            LastName=lname,
+            Department=dep,
+            Country=cou,
+        )
+
+        stdn.save()
+        return redirect("studentpage")
+    return render(request,'addstudentpage.html')
+
+def deletestudentpage(request,myid):
+    stdnt=StudentModel.objects.get(id=myid)
+    stdnt.delete()
+    return redirect("studentpage")
+
+def editstudentpage(request,myid):
+    stdnt=StudentModel.objects.filter(id=myid)
+    mydict={
+        'std':stdnt
+    }
+    return render(request,'editstudentpage.html',mydict)
+
+def updatestudentpage(request):
+    if request.method=='POST':
+        myid=request.POST.get('studentid')
+        fname=request.POST.get('firstname')
+        lname=request.POST.get('lastname')
+        dep=request.POST.get('department')
+        count=request.POST.get('country')
+
+        stdn=StudentModel(
+            id=myid,
+            FirstName=fname,
+            LastName=lname,
+            Department=dep,
+            Country=count,
+        )
+        stdn.save()
+        return redirect("studentpage")
+    
+
+
+

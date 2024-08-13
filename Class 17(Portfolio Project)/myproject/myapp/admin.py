68b44337bf614b8418fa56d6a31135ffd8347f9e
Tumpa Moni Mim <tumpamoni455@gmail.com>
2024-08-13 22:07:35 +0600

    Portfolio Project Complete

---
+++ Class 17(Portfolio Project)/myproject/myapp/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from myapp.models import *
+
+admin.site.register(SocialMediaModel),
+admin.site.register(AboutModel),
+admin.site.register(ClientModel),
+admin.site.register(SkillsModel),
+admin.site.register(SkillMatricesModel),
+admin.site.register(ResumeModel),
+admin.site.register(ResumeEducationModel),
+admin.site.register(ResumeProExModel),
+admin.site.register(PortfolioModel),
+admin.site.register(ServicesModel),
+admin.site.register(ServicesSectionModel),
+admin.site.register(TestimonialModel),
+admin.site.register(ClientTestimonialModel),
+admin.site.register(ContactModel),
